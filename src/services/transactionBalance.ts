7314dbc6da61a74de77268bbd5b76709ffd347cf
Temur <95220432+t1mch@users.noreply.github.com>
2023-11-12 20:54:07 +0500

    init by Temur

--- src/services/transactionBalance.ts
+++ src/services/transactionBalance.ts
@@ -5,43 +5,43 @@ import type { TransactionBalance } from '~/services/dto/transcationBalance'
 export const getTransactionBalance = (params: Record<any, any>): Promise<
   AxiosResponse<TransactionBalance>
 > => {
-  return $http.get('organization-balances?organization_id=7d28b7db-c6e1-442e-af46-c6635eca7748')
+  return $http.get('organization-balances?organization_id=8e636341-d899-40ee-8819-74edec19e6fc')
 }
 
 export const getOrganizationPartner = () => {
     return $http.get('organizations')
 }
 export const organizationWuthdrawal = (id: string) => {
-    return $http.get('organization-bank-accounts-search?organization_id=7d28b7db-c6e1-442e-af46-c6635eca7748')
+    return $http.get('organization-bank-accounts-search?organization_id=8e636341-d899-40ee-8819-74edec19e6fc')
 }
 export const getBlockedBalance = (params: Record<string, number>, id: string) => {
-    return $http.get('operations-search?organization_id=7d28b7db-c6e1-442e-af46-c6635eca7748&type[]=2&type[]=10&is_operated=null', {
+    return $http.get('operations-search?organization_id=8e636341-d899-40ee-8819-74edec19e6fc&type[]=2&type[]=10&is_operated=null', {
         params
     })
 }
 export const getOperations = (params: Record<string, number>, id: string) => {
-  return $http.get('operations-search?organization_id=7d28b7db-c6e1-442e-af46-c6635eca7748&', {
+  return $http.get('operations-search?organization_id=8e636341-d899-40ee-8819-74edec19e6fc&', {
     params,
   })
 }
 
 export const getBankingType = (params: Record<string, number>, id: string) => {
-    return $http.get('operations-search?organization_id=7d28b7db-c6e1-442e-af46-c6635eca7748&type[]=4&type[]=5', {
+    return $http.get('operations-search?organization_id=8e636341-d899-40ee-8819-74edec19e6fc&type[]=4&type[]=5', {
         params
     })
 }
 export const ErrorTransaction = (params: Record<string, number>, id: string) => {
-    return $http.get('operations-search?organization_id=7d28b7db-c6e1-442e-af46-c6635eca7748&type=5&status[]=22&status[]=33', {
+    return $http.get('operations-search?organization_id=8e636341-d899-40ee-8819-74edec19e6fc&type=5&status[]=22&status[]=33', {
         params
     })
 }
 export const getWithdrawal = (params: Record<string, number>, id: string) => {
-    return $http.get('operations-search?organization_id=7d28b7db-c6e1-442e-af46-c6635eca7748&type=5', {
+    return $http.get('operations-search?organization_id=8e636341-d899-40ee-8819-74edec19e6fc&type=5', {
         params
     })
 }
 export const getExpenses = (params: Record<string, number>, id: string) => {
-    return $http.get('operations-search?organization_id=7d28b7db-c6e1-442e-af46-c6635eca7748&type[]=6', {
+    return $http.get('operations-search?organization_id=8e636341-d899-40ee-8819-74edec19e6fc&type[]=6', {
         params
     })
 }
