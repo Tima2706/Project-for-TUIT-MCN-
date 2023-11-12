41328e61c4141ca5e929c78ff501af3c9d696c2b
Temur <95220432+t1mch@users.noreply.github.com>
2023-11-12 21:09:12 +0500

    init by Temur

--- src/components/pages/WithdrawFilterList.vue
+++ src/components/pages/WithdrawFilterList.vue
@@ -35,7 +35,7 @@ const DEFAULT_FILTER_DATA = {
   summa: '',
   account: '',
   note: '',
-  from_organization: '7d28b7db-c6e1-442e-af46-c6635eca7748'
+  from_organization: 'organization_id=8e636341-d899-40ee-8819-74edec19e6fc'
 }
 
 const form = ref<any>({ ...DEFAULT_FILTER_DATA })
