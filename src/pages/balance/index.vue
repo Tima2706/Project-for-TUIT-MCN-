2c5d9cec983ac4f55f6e2a796482a488be87fc38
Temur <95220432+t1mch@users.noreply.github.com>
2023-11-16 22:28:02 +0500

    init by Temur

--- src/pages/balance/index.vue
+++ src/pages/balance/index.vue
@@ -59,7 +59,7 @@ const params = reactive({
 const { data: transactionBalance, loading: transactionBalanceLoading } = useFetchData<TransactionBalance>(async () => {
   const { data } = await getTransactionBalance()
 
-  return {data: data}
+  return {data}
 }, { immediately: true })
 
 const { data: operations, loading: operationsLoading, fetch } = useFetchData<any[]>(async () => {
