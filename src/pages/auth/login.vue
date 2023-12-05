e1f033d692584c583620b5eb3e6fc2284b18785b
Temur <95220432+t1mch@users.noreply.github.com>
2023-12-05 21:53:54 +0500

    By Teemur

--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -1,26 +1,24 @@
 <script lang="ts" setup>
 import { TransitionSlide } from '@morev/vue-transitions'
 import { useToken } from '~/composables/useToken'
-// import {IS_DEV} from "~/utils/config";
+import {IS_DEV} from "~/utils/config";
 
 type FORM_TYPE = 'withKey' | 'withLoginAndPassword'
 const activeForm = ref<FORM_TYPE>('withKey')
 const router = useRouter()
 const route = useRoute()
 const { getOrganization } = useOrganizationStore()
-const { setToken,  } = useToken()
+const { setToken, gotoLogin } = useToken()
 const changeForm = (t: FORM_TYPE) => {
   activeForm.value = t
 }
 const loading = ref(true)
 
-// if (!getOrganization) {
-//
-// }
-// if (IS_DEV)
-//   loading.value = false
-// else
-//   gotoLogin()
+
+if (IS_DEV)
+  loading.value = false
+else
+  gotoLogin()
 
 const isLoginWithKeyFormActive = computed(() => activeForm.value === 'withKey')
 
